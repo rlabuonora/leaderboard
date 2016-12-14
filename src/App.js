41b0ae1d9dce4a1a838a5b92b54d7755d7eff91a
Rafael La Buonora <rlabuonora@yahoo.com>
2016-12-14 17:04:00 -0300

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+            <div className="row title">
+            <img src="https://s3.amazonaws.com/freecodecamp/freecodecamp_logo.svg" alt="freecodecamp-logo" />
+            </div>
+            <div className="main row">
+                <div className="row">
+            <h1> LeaderBoard </h1>
+            </div>
+            <div className="row">
+            <div className="col-xs-12">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Camper Name</th>
+                            <th>
+                                Points in past 30 days
+                                <i className="fa fa-sort-amount-desc active" aria-hidden="true"></i>
+                            </th>
+                            <th>
+                               All time Points
+                               <i className="fa fa-sort-amount-desc" aria-hidden="true"></i>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                            <td>
+                                <img src="https://avatars.githubusercontent.com/u/4639625?v=3" alt="user" className="userimage" />
+                                <a className="username" href="#">sjames1958gm</a>
+                            </td>
+                            <td>475</td>
+                            <td>39201</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                            <td>
+                                <img className="userimage" src="https://avatars.githubusercontent.com/u/4639625?v=3" alt="user" />
+                                <a className="username" href="#">sjames1958gm</a>
+                            </td>
+                            <td>475</td>
+                            <td>39201</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                            <td>
+                                <img className="userimage" src="https://avatars.githubusercontent.com/u/4639625?v=3" alt="user" />
+                                <a className="username" href="#">sjames1958gm</a>
+                            </td>
+                            <td>475</td>
+                            <td>39201</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            </div>
+            </div>
       </div>
     );
   }
